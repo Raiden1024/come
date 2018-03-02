361956138ce7bbed4daf8ac43939678a09d7dea0
Franck WIATROWSKI <franck@pegase.eveha.fr>
2018-03-02 14:08:21 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# COME (COnnect ME)
+## Synopsis
+Come is a small tool to help sysadmin connecting users machine over ssh. 
+
+It use the REST API plugin of an openfire server to display IP Address of users, verify online status or connect a user machine with SSH. 
+
+## Using "come"
+This git repository contain the binary "come" for linux platform. Copy it and launch it in command line. 
+
+## Licence
+3-Clauses BSD
---
+++ main.go
@@ -0,0 +1,190 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"os/exec"
+)
+
+// Config json
+type Config struct {
+	FireToken string `json:"fireToken"`
+	Host      string
+}
+
+// OpenfireSession ... Retrieve Session Datas
+type OpenfireSession struct {
+	Session OpenfireDatas `json:"session"`
+}
+
+// OpenfireSessions ... Retrieve Sessions Datas
+type OpenfireSessions struct {
+	Session []OpenfireDatas `json:"session"`
+}
+
+// OpenfireDatas ... Retrieve Sessions Fields
+type OpenfireDatas struct {
+	SessionID      string `json:"sessionId"`
+	Username       string `json:"username"`
+	Ressource      string `json:"ressource"`
+	Node           string `json:"node"`
+	SessionStatus  string `json:"sessionStatus"`
+	PresenceStatus string `json:"presenceStatus"`
+	Priority       string `json:"priority"`
+	HostAddress    string `json:"hostAddress"`
+	HostName       string `json:"hostName"`
+	CreationDate   string `json:"creationDate"`
+	LastActionDate string `json:"lastActionDate"`
+	Secure         bool   `json:"secure"`
+}
+
+var (
+	httpClient = &http.Client{}
+	arguments  = os.Args
+	config     = Config{}
+	proto      string
+)
+
+func init() {
+	if _, err := os.Stat(os.ExpandEnv("$HOME") + "/.config/comecfg.json"); err != nil {
+		fmt.Fprint(os.Stdout, "No configuration found.\nTo use 'come' you must have a functional OpenFire Server\nWith REST API plugin installed.\n")
+		fmt.Printf("openfire server address (format needed: 'http(s)://host(:port)': ")
+		buf := bufio.NewScanner(os.Stdin)
+		buf.Scan()
+		hostname := buf.Text()
+		fmt.Printf("openfire server API Secret Key: ")
+		buf.Scan()
+		token := buf.Text()
+		f, err := os.OpenFile(os.ExpandEnv("$HOME")+"/.config/comecfg.json", os.O_CREATE|os.O_WRONLY, 0664)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Could not write configuration: %v\n", err)
+		}
+		config = Config{
+			FireToken: token,
+			Host:      hostname,
+		}
+		newconfig, err := json.Marshal(&config)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Could not write configuration: %v\n", err)
+		}
+		f.Write(newconfig)
+		f.Close()
+		fmt.Println("Configuration created in " + os.ExpandEnv("$HOME") + "/.config/comecfg.json")
+	}
+	f, err := os.OpenFile(os.ExpandEnv("$HOME")+"/.config/comecfg.json", os.O_RDONLY, 0664)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Could not read configuration: %v", err)
+	}
+	reader, err := ioutil.ReadAll(f)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Could not read configuration: %v", err)
+	}
+	f.Close()
+	if err := json.Unmarshal(reader, &config); err != nil {
+		fmt.Fprintf(os.Stderr, "Could not read json file: %v", err)
+	}
+}
+
+func listSessions() error {
+	openfireSessions := OpenfireSessions{}
+	req, err := http.NewRequest("GET", config.Host+"/plugins/restapi/v1/sessions/", nil)
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Authorization", config.FireToken)
+	if err != nil {
+		return err
+	}
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	json.Unmarshal(body, &openfireSessions)
+	for _, v := range openfireSessions.Session {
+		fmt.Println(v.Username, v.HostAddress)
+	}
+	return nil
+}
+
+func connectMachine(userid string) (string, error) {
+	openfireSession := OpenfireSession{}
+	req, err := http.NewRequest("GET", config.Host+"/plugins/restapi/v1/sessions/"+userid, nil)
+	req.Header.Add("Accept", "application/json")
+	req.Header.Add("Authorization", config.FireToken)
+	if err != nil {
+		return "", err
+	}
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+	json.Unmarshal(body, &openfireSession)
+	return openfireSession.Session.HostAddress, nil
+}
+
+func main() {
+	if len(arguments) < 2 {
+		fmt.Println("Usage: come [ARGUMENT] [USER]\n-c ou c    SSH connection to a user machine, ex: come -c <user>\n-i ou i    Display User IP Address, ex: come -i <user>\n-w ou w    Wait for user Online status, ex: come -w <user>\n-l ou l    Display active sessions list (format: User IP)\n-h ou h    This help")
+	} else if len(arguments) > 3 {
+		fmt.Println("Too much parameters")
+	} else if len(arguments) == 2 {
+		if arguments[1] == "-h" || arguments[1] == "h" {
+			fmt.Println("Usage: come [ARGUMENT] [USER]\n-c ou c    SSH connection to a user machine, ex: come -c <user>\n-i ou i    Display User IP Address, ex: come -i <user>\n-w ou w    Wait for user Online status, ex: come -w <user>\n-l ou l    Display active sessions list (format: User IP)\n-h ou h    This help")
+		} else if arguments[1] == "-l" || arguments[1] == "l" {
+			err := listSessions()
+			if err != nil {
+				fmt.Printf("%v", err)
+			}
+		} else {
+			fmt.Println("Username missing")
+		}
+	} else if len(arguments) == 3 {
+		if arguments[1] == "-c" || arguments[1] == "c" {
+			ip, err := connectMachine(arguments[2])
+			if err != nil {
+				fmt.Printf("Unable to establish connection: %v", err)
+			}
+			fmt.Println("Waiting for connection...")
+			cmd := exec.Command("ssh", "root@"+ip)
+			cmd.Stdout = os.Stdout
+			cmd.Stdin = os.Stdin
+			cmd.Stderr = os.Stderr
+			cmd.Run()
+
+		} else if arguments[1] == "-w" || arguments[1] == "w" {
+			fmt.Println("Waiting for User Online Status...")
+			for {
+				ip, err := connectMachine(arguments[2])
+				if err != nil {
+					fmt.Printf("%v", err)
+				}
+				if ip != "" {
+					break
+				}
+			}
+			fmt.Println(arguments[2] + ": ONLINE")
+		} else if arguments[1] == "-i" || arguments[1] == "i" {
+			ip, err := connectMachine(arguments[2])
+			if err != nil {
+				fmt.Printf("Unable to obtain IP address: %v", err)
+			}
+			fmt.Printf("%s\n", ip)
+		} else {
+			fmt.Println("Unknown parameter, -h for help...")
+		}
+	}
+}
